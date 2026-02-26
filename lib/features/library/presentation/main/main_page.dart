aab85858a6bb6704ff49865e3631293e82f19f46
jmapps <jmappsmuslim@gmail.com>
2026-02-26 14:22:38 +0300

    Added main page and main state

---
+++ lib/features/library/presentation/main/main_page.dart
@@ -0,0 +1,91 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:salomon_bottom_bar/salomon_bottom_bar.dart';
+
+import '../../../../core/theme/app_styles.dart';
+import '../state/main_state.dart';
+import '../surahs/pages/surah_page.dart';
+
+class MainPage extends StatefulWidget {
+  const MainPage({super.key});
+
+  @override
+  State<MainPage> createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  late final ScrollController _scrollController = ScrollController();
+  late final List<Widget> _mainPages = [
+    SurahPage(scrollController: _scrollController),
+    SurahPage(scrollController: _scrollController),
+    SurahPage(scrollController: _scrollController),
+    SurahPage(scrollController: _scrollController),
+    SurahPage(scrollController: _scrollController),
+  ];
+
+  @override
+  void dispose() {
+    _scrollController.dispose();
+    super.dispose();
+  }
+
+  void _scrollToTop() {
+    _scrollController.animateTo(
+      0,
+      duration: const Duration(milliseconds: 500),
+      curve: Curves.easeIn,
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final appColors = Theme.of(context).colorScheme;
+    return Consumer<MainState>(
+      builder: (context, mainState, _) {
+        return Scaffold(
+          extendBody: true,
+          body: _mainPages[mainState.mainNavigatorIndex],
+          bottomNavigationBar: MediaQuery.removePadding(
+            context: context,
+            removeTop: true,
+            removeBottom: true,
+            child: Card(
+              margin: AppStyles.mainPadding,
+              shape: AppStyles.bigShape,
+              child: SalomonBottomBar(
+                itemShape: AppStyles.bigShape,
+                selectedItemColor: appColors.primary,
+                unselectedItemColor: appColors.onSurface.withAlpha(175),
+                items: [
+                  SalomonBottomBarItem(
+                    icon: const Icon(Icons.line_style_rounded),
+                    title: const Text('Суры'),
+                  ),
+                  SalomonBottomBarItem(
+                    icon: const Icon(Icons.circle_rounded),
+                    title: const Text('Джузы'),
+                  ),
+                  SalomonBottomBarItem(
+                    icon: const Icon(Icons.pages_rounded),
+                    title: const Text('Страницы'),
+                  ),
+                  SalomonBottomBarItem(
+                    icon: const Icon(Icons.settings_rounded),
+                    title: const Text('Настройки'),
+                  ),
+                ],
+                currentIndex: mainState.mainNavigatorIndex,
+                onTap: (int index) {
+                  if (mainState.mainNavigatorIndex != index) {
+                    mainState.mainNavigatorIndex = index;
+                  }
+                  _scrollToTop();
+                },
+              ),
+            ),
+          ),
+        );
+      },
+    );
+  }
+}
---
+++ lib/features/library/presentation/state/main_state.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class MainState extends ChangeNotifier {
+
+  int _mainNavigatorIndex = 0;
+
+  int get mainNavigatorIndex => _mainNavigatorIndex;
+
+  set mainNavigatorIndex(int index) {
+    _mainNavigatorIndex = index;
+    notifyListeners();
+  }
+}
