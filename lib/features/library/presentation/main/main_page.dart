c45c8defaa04abb726df61ba939df67051424b1f
jmapps <jmappsmuslim@gmail.com>
2026-03-07 15:35:48 +0300

    Added animation for screen switch and gradient for main screen bottom

--- lib/features/library/presentation/main/main_page.dart
+++ lib/features/library/presentation/main/main_page.dart
@@ -7,10 +7,7 @@ import '../../../../core/theme/app_styles.dart';
 import '../../../settings/pages/app_settings_page.dart';
 import '../hizb/pages/hizbs_page.dart';
 import '../juz/pages/juzs_page.dart';
-import '../state/hizb_state.dart';
-import '../state/juz_state.dart';
 import '../state/main_state.dart';
-import '../state/surah_state.dart';
 import '../surahs/pages/surahs_name_page.dart';
 
 class MainPage extends StatefulWidget {
@@ -26,14 +23,8 @@ class _MainPageState extends State<MainPage> {
   @override
   void initState() {
     super.initState();
-    Future.microtask(() {
-      if (mounted) {
-        context.read<SurahState>().loadAllSurahs();
-        context.read<JuzState>().loadAllJuzs();
-        context.read<HizbState>().loadAllHizbs();
-      }
-    });
   }
+
   late final List<Widget> _mainPages = [
     SurahNamePage(scrollController: _scrollController),
     SurahNamePage(scrollController: _scrollController),
@@ -59,56 +50,78 @@ class _MainPageState extends State<MainPage> {
   @override
   Widget build(BuildContext context) {
     final appColors = Theme.of(context).colorScheme;
-
-    final navIndex = context.select<MainState, int>(
-          (s) => s.mainNavigatorIndex,
-    );
-
+    final navIndex = context.select<MainState, int>((s) => s.mainNavigatorIndex);
     return Scaffold(
       extendBody: true,
-      body: _mainPages[navIndex],
+      body: AnimatedSwitcher(
+        duration: const Duration(milliseconds: 300),
+        transitionBuilder: (child, animation) {
+          return FadeTransition(
+            opacity: animation,
+            child: child,
+          );
+        },
+        child: KeyedSubtree(
+          key: ValueKey(navIndex),
+          child: _mainPages[navIndex],
+        ),
+      ),
       bottomNavigationBar: MediaQuery.removePadding(
         context: context,
         removeTop: true,
         removeBottom: true,
-        child: Card(
-          margin: AppStyles.mainPadding,
-          shape: AppStyles.bigShape,
-          child: SalomonBottomBar(
-            itemShape: AppStyles.bigShape,
-            selectedItemColor: appColors.primary,
-            unselectedItemColor: appColors.onSurface.withAlpha(175),
-            items: [
-              SalomonBottomBarItem(
-                icon: const Icon(Icons.line_style_rounded),
-                title: const Text(AppStrings.surahs),
-              ),
-              SalomonBottomBarItem(
-                icon: const Icon(Icons.bookmark_rounded),
-                title: const Text(AppStrings.bookmarks),
-              ),
-              SalomonBottomBarItem(
-                icon: const Icon(Icons.circle_rounded),
-                title: const Text(AppStrings.juzs),
-              ),
-              SalomonBottomBarItem(
-                icon: const Icon(Icons.pie_chart),
-                title: const Text(AppStrings.hizbs),
-              ),
-              SalomonBottomBarItem(
-                icon: const Icon(Icons.settings_rounded),
-                title: const Text(AppStrings.settings),
-              ),
-            ],
-            currentIndex: navIndex,
-            onTap: (int index) {
-              final state = context.read<MainState>();
-              if (navIndex != index) {
-                state.mainNavigatorIndex = index;
-              } else {
-                _scrollToTop();
-              }
-            },
+        child: Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+              colors: [
+                appColors.primary.withAlpha(0),
+                appColors.primary.withAlpha(5),
+                appColors.primary.withAlpha(15),
+                appColors.primary.withAlpha(25),
+                appColors.primary.withAlpha(55),
+              ],
+            ),
+          ),
+          child: Card(
+            margin: AppStyles.mainPadding,
+            shape: AppStyles.bigShape,
+            child: SalomonBottomBar(
+              itemShape: AppStyles.bigShape,
+              selectedItemColor: appColors.primary,
+              unselectedItemColor: appColors.onSurface.withAlpha(175),
+              items: [
+                SalomonBottomBarItem(
+                  icon: const Icon(Icons.line_style_rounded),
+                  title: const Text(AppStrings.surahs),
+                ),
+                SalomonBottomBarItem(
+                  icon: const Icon(Icons.bookmark_rounded),
+                  title: const Text(AppStrings.bookmarks),
+                ),
+                SalomonBottomBarItem(
+                  icon: const Icon(Icons.circle_rounded),
+                  title: const Text(AppStrings.juzs),
+                ),
+                SalomonBottomBarItem(
+                  icon: const Icon(Icons.pie_chart),
+                  title: const Text(AppStrings.hizbs),
+                ),
+                SalomonBottomBarItem(
+                  icon: const Icon(Icons.settings_rounded),
+                  title: const Text(AppStrings.settings),
+                ),
+              ],
+              currentIndex: navIndex,
+              onTap: (int index) {
+                if (navIndex != index) {
+                  context.read<MainState>().mainNavigatorIndex = index;
+                } else {
+                  _scrollToTop();
+                }
+              },
+            ),
           ),
         ),
       ),
