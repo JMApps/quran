e2c0386d141505000329c5f8b46ae671657cd4e4
jmapps <jmappsmuslim@gmail.com>
2026-03-22 18:37:11 +0300

    Update all android libraries

---
+++ android/app/build.gradle.kts
@@ -0,0 +1,44 @@
+plugins {
+    id("com.android.application")
+    id("kotlin-android")
+    id("dev.flutter.flutter-gradle-plugin")
+}
+
+android {
+    namespace = "com.taqweem.quran.quran"
+    compileSdk = 36
+    ndkVersion = "30.0.14904198"
+
+    compileOptions {
+        isCoreLibraryDesugaringEnabled = true
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlinOptions {
+        jvmTarget = JavaVersion.VERSION_17.toString()
+    }
+
+    defaultConfig {
+        multiDexEnabled = true
+        applicationId = "com.taqweem.quran.quran"
+        minSdk = 26
+        targetSdk = 36
+        versionCode = flutter.versionCode
+        versionName = flutter.versionName
+    }
+
+    buildTypes {
+        release {
+            signingConfig = signingConfigs.getByName("debug")
+        }
+    }
+}
+
+flutter {
+    source = "../.."
+}
+
+dependencies {
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.1.5")
+}
---
+++ android/app/src/main/kotlin/com/taqweem/quran/quran/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.taqweem.quran.quran
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
