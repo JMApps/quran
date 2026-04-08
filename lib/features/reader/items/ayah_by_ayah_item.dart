7468474fae777b46f70d1bedf3192bcffe4e6815
jmapps <jmappsmuslim@gmail.com>
2026-04-08 15:39:49 +0300

    Added ayah item options

--- lib/features/reader/items/ayah_by_ayah_item.dart
+++ lib/features/reader/items/ayah_by_ayah_item.dart
@@ -19,58 +19,101 @@ class AyahByAyahItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final appColors = Theme.of(context).colorScheme;
-    return Container(
-      padding: AppStyles.vrBigHrMiniPadding,
-      decoration: const BoxDecoration(
-        border: Border.symmetric(
-          horizontal: BorderSide(
-            width: 0.25,
-            color: Colors.grey
-          )
-        )
-      ),
-      child: Column(
-        crossAxisAlignment: .start,
-        children: [
-          Container(
-            width: 65,
-            padding: AppStyles.microPadding,
-            alignment: Alignment.center,
-            decoration: BoxDecoration(
-              color: appColors.secondaryContainer.withAlpha(155),
-              borderRadius: AppStyles.miniBorder,
-            ),
-            child: Text(ayahByAyahModel.verseKey),
-          ),
-          const SizedBox(height: 16),
-          Consumer<AppSettingsState>(
-            builder: (context, appSettingsState, _) {
-              return Column(
+    return GestureDetector(
+      onLongPress: () {
+        showModalBottomSheet(
+          context: context,
+          builder: (ctx) {
+            return Padding(
+              padding: AppStyles.withoutTopPadding,
+              child: Column(
+                mainAxisSize: .min,
                 crossAxisAlignment: .stretch,
                 children: [
-                  Text(
-                    ayahByAyahModel.ayahArabic,
-                    textDirection: TextDirection.rtl,
-                    style: TextStyle(
-                      fontSize: appSettingsState.ayahArabicTextSize,
-                      fontFamily: AppStrings.fontUthmanicHafs,
-                      height: 2.25,
-                      letterSpacing: 0,
-                    ),
+                  TextButton.icon(
+                    onPressed: () {},
+                    iconAlignment: .end,
+                    label: Text('Добавить в избранное', style: AppStyles.mainTextStyle16,),
+                    icon: const Icon(Icons.bookmark),
                   ),
-                  const SizedBox(height: 16),
-                  Text(
-                    ayahByAyahModel.ayahTranslation,
-                    style: TextStyle(
-                      fontSize: appSettingsState.ayahTranslationTextSize,
-                      fontFamily: AppStrings.fontGilroy,
-                    ),
+                  const Divider(),
+                  TextButton.icon(
+                    onPressed: () {},
+                    iconAlignment: .end,
+                    label: Text('Скопировать', style: AppStyles.mainTextStyle16,),
+                    icon: const Icon(Icons.content_copy_rounded),
+                  ),
+                  const Divider(),
+                  TextButton.icon(
+                    onPressed: () {},
+                    iconAlignment: .end,
+                    label: Text('Поделиться', style: AppStyles.mainTextStyle16,),
+                    icon: const Icon(Icons.ios_share_rounded),
                   ),
                 ],
-              );
-            },
+              ),
+            );
+          },
+        );
+      },
+      child: Container(
+        padding: AppStyles.vrBigHrMiniPadding,
+        decoration: const BoxDecoration(
+          border: Border.symmetric(
+            horizontal: BorderSide(
+              width: 0.25,
+              color: Colors.grey,
+            ),
           ),
-        ],
+        ),
+        child: Column(
+          crossAxisAlignment: .start,
+          children: [
+            const SizedBox(height: 14),
+            Row(
+              mainAxisAlignment: .start,
+              children: [
+                Container(
+                  width: 65,
+                  padding: AppStyles.microPadding,
+                  alignment: Alignment.center,
+                  decoration: BoxDecoration(
+                    color: appColors.secondaryContainer.withAlpha(155),
+                    borderRadius: AppStyles.miniBorder,
+                  ),
+                  child: Text(ayahByAyahModel.verseKey),
+                ),
+              ],
+            ),
+            Consumer<AppSettingsState>(
+              builder: (context, appSettingsState, _) {
+                return Column(
+                  crossAxisAlignment: .stretch,
+                  children: [
+                    Text(
+                      ayahByAyahModel.ayahArabic,
+                      textDirection: .rtl,
+                      style: TextStyle(
+                        fontSize: appSettingsState.ayahArabicTextSize,
+                        fontFamily: AppStrings.fontUthmanicHafs,
+                        height: 2.25,
+                        letterSpacing: 0,
+                      ),
+                    ),
+                    const SizedBox(height: 14),
+                    Text(
+                      ayahByAyahModel.ayahTranslation,
+                      style: TextStyle(
+                        fontSize: appSettingsState.ayahTranslationTextSize,
+                        fontFamily: AppStrings.fontGilroy,
+                      ),
+                    ),
+                  ],
+                );
+              },
+            ),
+          ],
+        ),
       ),
     );
   }
